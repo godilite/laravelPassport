d949706b2bb1254d3b2198a35881be632e276ed3
Godwin Asuquo <godilite@gmail.com>
2020-02-07 16:42:17 +0100

    Feature: Created Fresh Laravel Installation

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+        public function login()
+        {
+            if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+                $user = Auth::user();
+                $success['token'] = $user->createToken('appToken')->accessToken;
+               //After successfull authentication, notice how I return json parameters
+                return response()->json([
+                  'success' => true,
+                  'token' => $success,
+                  'user' => $user
+              ]);
+            } else {
+           //if authentication is unsuccessfull, notice how I return json parameters
+              return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+            }
+        }
+
+         /**
+     * Register api.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fname' => 'required',
+            'lname' => 'required',
+            'phone' => 'required|unique:users|regex:/(0)[0-9]{10}/',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+          return response()->json([
+            'success' => false,
+            'message' => $validator->errors(),
+          ], 401);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('appToken')->accessToken;
+        return response()->json([
+          'success' => true,
+          'token' => $success,
+          'user' => $user
+      ]);
+    }
+
+    public function logout(Request $res)
+    {
+      if (Auth::user()) {
+        $user = Auth::user()->token();
+        $user->revoke();
+
+        return response()->json([
+          'success' => true,
+          'message' => 'Logout successfully'
+      ]);
+      }else {
+        return response()->json([
+          'success' => false,
+          'message' => 'Unable to Logout'
+        ]);
+      }
+     }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/login', 'UsersController@login');
+    Route::post('/register', 'UsersController@register');
+    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
+});
